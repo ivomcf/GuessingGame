606fd7c358b7539e01ad80c43e1dda899727d8fb
ivomcf <32466992+ivomcf@users.noreply.github.com>
2020-04-19 11:35:12 +0100

    Validação

--- app/src/main/java/pt/ipg/guessinggame/MainActivity.java
+++ app/src/main/java/pt/ipg/guessinggame/MainActivity.java
@@ -1,8 +1,13 @@
 package pt.ipg.guessinggame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
+import java.util.Scanner;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void readData(View view) {
+
+        EditText editTextInput = (EditText) findViewById(R.id.editTextInput);
+
+        String StrInput = editTextInput.getText().toString();
+        int input = -1;
+        try {
+            input = Integer.parseInt(StrInput);
+        } catch (NumberFormatException e){
+            e.printStackTrace();
+        }
+
+        Random rand = new Random();
+        int guessedNumber = rand.nextInt(10);
+        int numberOfTries = 0;
     }
 }
